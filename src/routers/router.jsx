b70893f8ad4aab5d459c616664e364153f957e42
nayan97 <nayanislam085@gmail.com>
2025-07-07 23:48:15 +0600

    header and footer is working fine

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,10 +1,17 @@
 import {
   createBrowserRouter,
 } from "react-router";
+import Layout from "../Layouts/Layout";
+import Home from "../pages/Home/Home";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: <div>Hello World</div>,
+Component: Layout,
+children: [
+    {
+        index: true,
+        Component: Home,
+    }
+]
   },
 ]);
