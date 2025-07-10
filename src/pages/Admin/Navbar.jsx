68ca5b854bcaca1ccab3f2dbf9e039d94572bf38
nayan97 <nayanislam085@gmail.com>
2025-07-10 10:24:16 +0600

    created navbar on admin

--- src/pages/Admin/Navbar.jsx
+++ src/pages/Admin/Navbar.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React from "react";
+import { Menu } from "lucide-react";
+import useAuth from "../../hooks/useAuth";
 
-const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Navbar = ({ toggleSidebar }) => {
+  const { user,  logoutUser } = useAuth();
+  console.log(user);
+
+  const handleLogout = () => {
+    logoutUser()
+      .then(() => {
+        console.log("logout successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div className="navbar bg-black border-b justify-between px-4 text-white">
+      <button onClick={toggleSidebar} className="btn btn-ghost">
+        <Menu />
+      </button>
+      <div className="dropdown dropdown-end">
+        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+          <div className="w-10 rounded-full">
+            <img src="/img/avatar5.png" alt="Avatar" />
+          </div>
+        </label>
+        <ul
+          tabIndex={0}
+          className="menu menu-sm dropdown-content mt-3 z-[1] p-2 bg-base-100 text-black rounded-box w-52"
+        >
+          <li>
+            <a>Settings</a>
+          </li>
+          <li>
+            {" "}
+            <a href="/login" className="" onClick={handleLogout}>
+              Logout
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
